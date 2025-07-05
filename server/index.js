ceabaa34d8e9ae965b2a3deaa270cd3290cc241f
techdivines <129510899+techdivines@users.noreply.github.com>
2025-07-06 01:02:41 +0530

    Database Code

--- server/index.js
+++ server/index.js
@@ -13,5 +13,5 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("API Running..."));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
