ee622bf2138997374b9fec4a098cfb04f106ccb7
techdivines <129510899+techdivines@users.noreply.github.com>
2025-07-06 00:57:56 +0530

    Database Code

--- server/index.js
+++ server/index.js
@@ -1,21 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import connectDB from './src/config/db.js'; // <-- import DB connection function
 
-dotenv.config();
+dotenv.config(); // Load environment variables
+
+connectDB(); // Connect to MongoDB
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-
 app.get("/", (req, res) => res.send("API Running..."));
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
